87d964766942647a068757d9f07f29cb1d5e0763
Wahab-dev09 <wahabzsec@gmail.com>
2024-09-30 00:43:37 -0600

    Paths Corrected

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Load all the sound files
-const bellSound = new Audio('/bell.wav');
-const drumSound = new Audio('/drum.wav');
-const popSound = new Audio('/pop.wav');
-const clickSound = new Audio('/click.wav');
-const losing = new Audio('/Losing.wav');
-const cat = new Audio('/Cat.wav');
+const bellSound = new Audio('/sounds/bell.wav');
+const drumSound = new Audio('/sounds/drum.wav');
+const popSound = new Audio('/sounds/pop.wav');
+const clickSound = new Audio('/sounds/click.wav');
+const losing = new Audio('/sounds/Losing.wav');
+const cat = new Audio('/sounds/Cat.wav');
 
 // Add event listeners to buttons and play respective sounds
 document.getElementById('bell').addEventListener('click', () => {
