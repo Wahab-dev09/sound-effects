5ee22dd518488d3f55cc3730f7f1f13815454caf
Wahab-dev09 <wahabzsec@gmail.com>
2024-09-30 01:04:16 -0600

    Deployment Settings

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Load all the sound files
-const bellSound = new Audio(`${import.meta.env.BASE_URL}sounds/bell.wav`);
-const drumSound = new Audio('/sounds/drum.wav');
-const popSound = new Audio('/sounds/pop.wav');
-const clickSound = new Audio('/sounds/click.wav');
-const losing = new Audio('/sounds/Losing.wav');
-const cat = new Audio('/sounds/Cat.wav');
+const bellSound = new Audio('/sound-effects/sounds/bell.wav');
+const drumSound = new Audio('/sound-effects/sounds/drum.wav');
+const popSound = new Audio('/sound-effects/sounds/pop.wav');
+const clickSound = new Audio('/sound-effects/sounds/click.wav');
+const losing = new Audio('/sound-effects/sounds/Losing.wav');
+const cat = new Audio('/sound-effects/sounds/Cat.wav');
 
 // Add event listeners to buttons and play respective sounds
 document.getElementById('bell').addEventListener('click', () => {
